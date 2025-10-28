3b73c23646e85e0ee05a1d9e69272e0e0b7a5b38
Алексей Семионов <alehaaaaa@mail.ru>
2025-10-28 16:15:19 +0300

    Departments DONE

--- MVC/ContosoUniversity/Models/Instructor.cs
+++ MVC/ContosoUniversity/Models/Instructor.cs
@@ -1,34 +1,38 @@
-﻿using Microsoft.SqlServer.Server;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-	public class Student
+	public class Instructor
 	{
 		public int ID { get; set; }
+
 		[Required]
-		[DisplayName("Фамилия")]
+		[Display(Name ="Фамилия")]
 		[StringLength(24)]
 		[RegularExpression(@"^[A-Z]+[a-z]*$")]
 		public string LastName { get; set; }
+
 		[Required]
-		[Display(Name = "Имя")]
+		[Display(Name ="Имя")]
 		[StringLength(24)]
 		[RegularExpression(@"^[A-Z]+[a-z]*$")]
 		public string FirstName { get; set; }
+
+
 		[Required]
-		[Display(Name = "Дата поступления")]
 		[DataType(DataType.Date)]
-		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-		public DateTime EnrollmentDate { get; set; }    //Дата поступления
+		[DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}")]
+		[Display(Name ="Work since")]
+		public DateTime HireDate { get; set; }
 
-
-		[DisplayName("Student")]
+		[Display(Name ="Teacher")]
 		public string FullName { get => $"{LastName}{FirstName}"; }
 
-		//Navigation property:
-		public ICollection<Enrollment> Enrollments { get; set; }
+		//Navigation properties:
+		public ICollection<CourseAssignment> CourseAssignments { get; set; }
+		public OfficeAssignment OfficeAssignment { get;set; }
 	}
 }
+
